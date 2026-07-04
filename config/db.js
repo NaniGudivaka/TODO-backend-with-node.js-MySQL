8f9b76c3779fbe0d65f1520ca413d4b8414cbc32
NaniGudivaka <nanigudivaka0804@gmail.com>
2026-07-04 21:11:44 +0530

    creating tables if not exists

--- config/db.js
+++ config/db.js
@@ -17,7 +17,47 @@ const pool = mysql.createPool({
   queueLimit: 0,
 });
 
+//creating table
 
+async function createTable() {
+  await pool.query(`
+    create table if not exists users(
+    id int primary key auto_increment,
+    name varchar(100) not null,
+    email varchar(150) unique not null,
+
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp on update current_timestamp 
+    )
+    `);
+
+    await pool.query(`
+      create table if not exists todos(
+      id int primary key auto_increment,
+      user_id int not null,
+      tasks varchar(500) not null,
+      completed boolean default false,
+
+      created_at timestamp default current_timestamp,
+      updated_at timestamp default current_timestamp on update current_timestamp,
+
+      foreign key(user_id) references users(id)
+      on delete cascade 
+      )
+      `);
+}
+
+const tableConnection = async () =>{
+  try{
+    await createTable();
+    console.log('Tables created successfully');
+
+  }catch(error){
+    console.log(error);
+  }
+}
+
+tableConnection();
 
 module.exports = pool;
 
